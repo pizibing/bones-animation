a954ee1624533241517874441ccec4403b7e60af
andrewRen <andrewRen@localhost>
2009-06-30 08:49:46 +0800

    light,texture,rotate,
    reviewed by xulingjing

--- src/calculation/Calculator.cpp
+++ src/calculation/Calculator.cpp
@@ -1,9 +1,30 @@
 #include "Calculator.h"
 
-Calculator::Calculator(void)
-{
+Calculator::Calculator(void){
+	meshManager = SimpleMeshManager::getInstance();
 }
 
 Calculator::~Calculator(void)
 {
+}
+
+void Calculator::rotate(float angle,int axis){
+	Mesh* mesh = meshManager->getMesh(0,0);
+	glPushMatrix();
+	glLoadMatrixf(mesh->matrix);
+	switch(axis){
+		case AXIS_X:
+			glRotatef(angle,1,0,0);
+			break;
+		case AXIS_Y:
+			glRotatef(angle,0,1,0);
+			break;
+		case AXIS_Z:
+			glRotatef(angle,0,0,1);
+			break;
+		default:
+			return;
+	}
+	glGetFloatv(GL_MODELVIEW_MATRIX,mesh->matrix);
+	glPopMatrix();
 }
