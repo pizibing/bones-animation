5b5dced6898b6dcf01511b72d5d35894dae521f8
xljacwy <xljacwy@yahoo.com.cn>
2009-07-02 13:30:01 +0800

    ?????? Confirmed by yangwei.

--- src/loaders/FColladaModelLoader.h
+++ src/loaders/FColladaModelLoader.h
@@ -6,10 +6,22 @@
 #pragma warning( disable : 4996 )
 
 #include "FCollada.h"
+
+// FCollada
 #include "FCDocument\FCDocument.h"
+#include "FCDocument\FCDLibrary.h"
+#include "FCDocument\FCDGeometryMesh.h"
+#include "FCDocument\FCDGeometry.h"
+#include "FCDocument\FCDGeometryPolygons.h"
+#include "FCDocument\FCDGeometrySource.h"
+#include "FCDocument\FCDSceneNode.h" 
+#include "FCDocument\FCDGeometryPolygonsTools.h"
+#include "FCDocument\FCDGeometryPolygonsInput.h"
+#include "FUtils\FUObject.h" 
 
 #include "ModelLoader.h"
 #include "../managers/MeshManager.h"
+#include "../data/MyPolygon.h"
 
 class FColladaModelLoader :
 	public ModelLoader
@@ -18,12 +30,15 @@ public:
 	FColladaModelLoader(void);
 	~FColladaModelLoader(void);
 
-	// pointer to dae file taht will be opened using fcollada
-	FCDocument* m_document;
-
 	bool loadModel(int kind, char* path);
 
 private:
 	bool m_is_file_opened; 
+
+	// pointer to dae file that will be opened using fcollada
+	FCDocument* m_document;
+
+	int m_num_geometries;
+
 	MeshManager* meshManager;
 };
