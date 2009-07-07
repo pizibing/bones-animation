467b8cfdb8e65cc21a4374f8081a1cdd8e5cb6e3
YangWei <YangWei@localhost>
2009-07-07 17:03:25 +0800

    add skeleton,bone,animation,track,keyframe definition there some general comments for every class Reviewed by RenQingjie

---
+++ src/data/character/ChSkeleton.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Skeleton for a character
+// contains the pointers of all bones
+// must set bone total number when create a new skeleton
+
+
+class ChSkeleton
+{
+public:
+	ChSkeleton(void);
+	~ChSkeleton(void);
+
+};
