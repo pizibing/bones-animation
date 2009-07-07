e4d4fb3f4239b0cd86affdce1fa50666063a3216
andrewRen <andrewRen@localhost>
2009-07-07 22:14:52 +0800

    correct bug : CharacterObject

--- src/data/character/CharacterObject.cpp
+++ src/data/character/CharacterObject.cpp
@@ -7,3 +7,21 @@ CharacterObject::CharacterObject(void){
 
 CharacterObject::~CharacterObject(void){
 }
+
+// return a list of VBOMeshes that this VBOObject represent
+// this function return a pointer to the first VBOMesh of the 
+// list and num will be changed to the size of the list
+VBOMesh* CharacterObject::representInVBOMesh(int* num){
+	*num = meshSize;
+	return vbomeshes;
+}
+
+//return the type of this object
+int CharacterObject::getType(){
+	return type;
+}
+
+//return the id of this object
+int CharacterObject::getID(){
+	return id;
+}
