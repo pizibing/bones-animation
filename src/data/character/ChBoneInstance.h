17de9366ceccec39a3a9c8d9984f6ffcedb53d56
andrewRen <andrewRen@localhost>
2009-07-07 17:02:52 +0800

    added: ChBoneInstance ChSkin ChSkinInstance ChVertex

---
+++ src/data/character/ChBoneInstance.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// ChBoneInstance is a class that stores the current instance
+// of a character's skeleton.
+// this instance will be changed every frame according to the
+// animation.
+class ChBoneInstance
+{
+public:
+	// constructor
+	ChBoneInstance(void);
+	// destructor
+	~ChBoneInstance(void);
+};
