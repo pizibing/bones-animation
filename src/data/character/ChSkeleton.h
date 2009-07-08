d1a738985d59bab5c113ecf199280637f9a9d1ce
YangWei <YangWei@localhost>
2009-07-08 10:19:52 +0800

    add ChAnimationManager class to manage all animations
    Reviewed by RenQingjie

--- src/data/character/ChSkeleton.h
+++ src/data/character/ChSkeleton.h
@@ -4,11 +4,22 @@
 // contains the pointers of all bones
 // must set bone total number when create a new skeleton
 
+#include <map>
+
+class ChBone;
 
 class ChSkeleton
 {
 public:
+	// constructor
 	ChSkeleton(void);
+	// destructor
 	~ChSkeleton(void);
+	
+
+private:
+	ChBone (*m_bones)[];
+	std::map< char*, int > m_boneNameMap;
+	ChBone * m_rootBone;
 
 };
