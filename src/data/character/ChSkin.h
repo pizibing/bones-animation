6d3c33dede4763d4f4488ffaaac42cedeb6f31dd
andrewRen <andrewRen@localhost>
2009-07-10 10:49:37 +0800

    modified: ChSkin,ChVertex           Chskin: functions of ChSkin           ChVertex: add property id and its get and set function confirmed by yangwei

--- src/data/character/ChSkin.h
+++ src/data/character/ChSkin.h
@@ -12,7 +12,18 @@ public:
 	// destructor
 	~ChSkin(void);
 
+	// initialize vertices
+	// if vertices has never been initialized, a new array
+	// of ChVertex will be created whose length is num
+	// else nothing will happen
+	void initVertices(int num);
+
+	// set the verNum-th ChVertex of vertices to vertex
+	// verNum should be smaller than num
+	// vertex's id will be set to verNum
+	void initVertice(int verNum, ChVertex* vertex);
+
 private:
 	// a pointer to an array that stores all the vertices
-	ChVertex* vertices;
+	ChVertex** vertices;
 };
