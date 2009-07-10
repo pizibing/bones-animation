01732c59db7b424dd65d3c7d454905b7bfe01f2e
andrewRen <andrewRen@localhost>
2009-07-10 10:01:18 +0800

    added: ChSkeletonInstance modified: CharacterObject.h   define the initial of CharacterObject
    confirmed by yangwei

--- src/data/character/ChBoneInstance.h
+++ src/data/character/ChBoneInstance.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // ChBoneInstance is a class that stores the current instance
-// of a character's skeleton.
+// of a character's bone.
 // this instance will be changed every frame according to the
 // animation.
 class ChBoneInstance
