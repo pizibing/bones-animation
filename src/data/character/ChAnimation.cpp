6465b69b82900b349012f19679876535734b5089
YangWei <YangWei@localhost>
2009-07-10 15:41:39 +0800

    implemented class ChBone and ChSkeleton ReViewed by RenQingjie

---
+++ src/data/character/ChAnimation.cpp
@@ -0,0 +1,45 @@
+#include "../matrixlib/mtxlib.h"
+//#include "../matrixlib/matrix.h"
+#include "ChAnimation.h"
+#include "ChBone.h"
+#include "ChSkeleton.h"
+#include "ChTrack.h"
+
+ChAnimation::ChAnimation(void){}
+
+ChAnimation::~ChAnimation(void){}
+
+// set skeleton and init the animation
+// and initialize track of bone
+void ChAnimation::setSkeleton(ChSkeleton * skeleton){
+	m_skeleton = skeleton;
+	init(m_skeleton->getBoneNum());
+}
+
+// @return the track with the bonename
+ChTrack * ChAnimation::getTrack(std::string bonename){
+	return NULL;
+}
+
+// @return the track with the boneId
+ChTrack * ChAnimation::getTrack(int boneId){
+	return NULL;
+}
+// initialize the track array
+bool ChAnimation::init(int track_num){
+	return false;
+}
+
+// calculate the transform matrix for the bone
+// @param time_ms the time in million second from the beginning of the
+//        animation
+// @param boneId the index for the blending bone
+// @return the transform matrix for the bone
+const Matrix& ChAnimation::blendBone(int time_ms,int boneId){
+	return m_tracks[boneId]->getTransformMatrix(time_ms);
+}
+
+// @return the total time of the animation;
+int ChAnimation::getAnimationTime(){
+	return 0;
+}
---
+++ src/data/character/ChKeyFrame.cpp
@@ -0,0 +1,35 @@
+#include "../matrixlib/mtxlib.h"
+//#include "../matrixlib/matrix.h"
+#include "ChKeyFrame.h"
+
+// default constructor
+ChKeyFrame::ChKeyFrame(void){}
+
+// constructor
+// @param matrix the transform matrix
+ChKeyFrame::ChKeyFrame(const Matrix &matrix, int frame_time){}
+
+// destructor
+ChKeyFrame::~ChKeyFrame(void){}
+
+// @return the transform matrix
+inline const Matrix & ChKeyFrame::getTransformMatrix()const{
+	return m_transformMatrix;
+}
+
+// set the transform matrix
+inline void ChKeyFrame::setTransformMatrix(const Matrix & matrix){
+	m_transformMatrix = matrix;
+}
+
+// @return the start time(ms) of the frame
+inline int ChKeyFrame::getTime(){
+	return m_time;
+}
+
+// set the total time(ms) of the frame
+inline void ChKeyFrame::setTime(int time){
+	m_time = time;
+}
+
+
