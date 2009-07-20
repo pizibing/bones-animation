20a8acbec7ae51e21ef83b300007150ecb6ae878
winkinglee-vip <winkinglee-vip@163.com>
2009-07-20 10:07:40 +0800

    test.doc LiWenqi

---
+++ src/data/character/ChVBOMesh.h
@@ -0,0 +1,48 @@
+#pragma once
+
+class VBOMesh;
+class ChSkin;
+class ChSkinInstance;
+
+// this class is the CharacterObject version VBOMesh
+// it not only stores the VBOMesh but also the vertices
+// the this VBOMesh is related to
+class ChVBOMesh
+{
+public:
+	// constructor
+	// create a VBOMesh , set its vertices to default
+	// position of vertices whose ids is in vertices
+	// size is the length of vertices
+	ChVBOMesh(const int* vertices, int size, ChSkin* skin);
+	// destructor
+	~ChVBOMesh(void);
+
+	// get function of vbomesh
+	VBOMesh* getVBOMesh();
+
+	// update the vertices of VBOMesh according to the ids
+	// contained in vertices.
+	// ChVertexInstance is used here as the vertex the id 
+	// relates to.
+	void updateVBO(ChSkinInstance* skin);
+
+	// update the matrix of the VBOMesh to the given matrix
+	void updateVBO(float* matrix);
+
+private:
+	// the VBOMesh this class packed
+	VBOMesh* vbomesh;
+	// vertices' ids
+	int* verticesId;
+	// store the position of vertices in the following format
+	// x1,y1,z1,x2,y2,z2,x3,y3,z3.....
+	float* vertices;
+	// store the position of vertices in the following format
+	// x1,y1,z1,x2,y2,z2,x3,y3,z3.....
+	float* normals;
+	// whether this vbomesh has normal
+	bool hasNormal;
+	// size of vertices, vbomesh's size should be 3 times of size
+	int size;
+};
