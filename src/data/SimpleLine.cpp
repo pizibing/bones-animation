fb7be2fa2c2a3092280155787750b52e41f98b45
YangWei <YangWei@localhost>
2009-07-22 13:24:30 +0800

    implemented bone line representation for character
    Reviewed by RenQingjie

--- src/data/SimpleLine.cpp
+++ src/data/SimpleLine.cpp
@@ -1,14 +1,6 @@
 #include <assert.h>
 #include "SimpleLine.h"
 
-// constructor
-SimpleLine::SimpleLine(void){
-}
-
-// destructor
-SimpleLine::~SimpleLine(void){
-}
-
 // set function of dot1
 void SimpleLine::setDot1(const float* dot1){
 	// dot1 should not be null
@@ -27,15 +19,5 @@ void SimpleLine::setDot2(const float* dot2){
 	// set x,y,z
 	for(int i = 0; i < 3; i++)
 		this->dot2[i] = dot2[i];
-}
-
-// get function of dot1
-const float* SimpleLine::getDot1(){
-	return dot1;
-}
-
-// get function of dot2
-const float* SimpleLine::getDot2(){
-	return dot2;
 }
 
