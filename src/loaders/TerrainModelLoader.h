42bfe0fda15977f6132de7b76ba5bdf3161e4470
andrewRen <andrewRen@localhost>
2009-07-22 18:38:24 +0800

    complete TerrainObject
    confirmed by yangwei

---
+++ src/loaders/TerrainModelLoader.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../loaders/ModelLoader.h"
+
+// TerrainModelLoader load terrain object
+class TerrainModelLoader :
+	public ModelLoader
+{
+public:
+	// constructor
+	TerrainModelLoader(void);
+	// destructor
+	~TerrainModelLoader(void);
+
+	/*
+	* para: path: load a model with the given path
+	*       kind: specifies the role of the model in the entire world(e.g terrain,character etc.)
+	* function: load the model with the given path and set it in the ObjectManager
+	* return: true when model is successfully loaded, vice versa
+	*/
+	bool loadModel(int kind, const char* path);
+};
