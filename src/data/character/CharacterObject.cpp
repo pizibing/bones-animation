b3d1ca2692ccbceb71a248f8e23a3c8eb4c210b5
andrewRen <andrewRen@localhost>
2009-07-24 14:52:58 +0800

    modified Calculator : support idle animation
    confirmed by xulingjing

--- src/data/character/CharacterObject.cpp
+++ src/data/character/CharacterObject.cpp
@@ -243,6 +243,10 @@ void CharacterObject::setGesture(const char* animation, int time){
 	for(int i = 0; i < meshSize; i++){
 		vbomeshes[i] = *chvbomeshes[i]->getVBOMesh();
 	}
+	//
+	for(int i = 0; i < 16; i++)
+		printf("%f ",change[i]);
+	printf("\n");
 }
 
 // set the current gesture of the character
