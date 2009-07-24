8cff724dd1377930cd1ef875ee0f175d819424b5
YangWei <YangWei@localhost>
2009-07-24 15:03:49 +0800

    resolved matrix and quternion to right index
    Reivewed by RenQingjie

--- src/matrixlib/matrix.cpp
+++ src/matrixlib/matrix.cpp
@@ -21,26 +21,22 @@ Matrix Matrix::operator + (const Matrix & matrix) const{
 // @return a matrix for the result of multiplication of this and another
 Matrix Matrix::operator * (const Matrix & matrix) const{
 	Matrix mx;
-	// row 0
-	mx.m[0] = m[0] * matrix.m[0] + m[1] * matrix.m[4] + m[2] * matrix.m[8] + m[3] * matrix.m[12];
-	mx.m[4] = m[4] * matrix.m[0] + m[5] * matrix.m[4] + m[6] * matrix.m[8] + m[7] * matrix.m[12];
-	mx.m[8] = m[8] * matrix.m[0] + m[9] * matrix.m[4] + m[10] * matrix.m[8] + m[11] * matrix.m[12];
-	mx.m[12] = m[12] * matrix.m[0] + m[13] * matrix.m[4] + m[14] * matrix.m[8] + m[15] * matrix.m[12];
-	// row 1
-	mx.m[1] = m[0] * matrix.m[1] + m[1] * matrix.m[5] + m[2] * matrix.m[9] + m[3] * matrix.m[13];
-	mx.m[5] = m[4] * matrix.m[1] + m[5] * matrix.m[5] + m[6] * matrix.m[9] + m[7] * matrix.m[13];
-	mx.m[9] = m[8] * matrix.m[1] + m[9] * matrix.m[5] + m[10] * matrix.m[9] + m[11] * matrix.m[13];
-	mx.m[13] = m[12] * matrix.m[1] + m[13] * matrix.m[5] + m[14] * matrix.m[9] + m[15] * matrix.m[13];
-	// row 2
-	mx.m[2] = m[0] * matrix.m[2] + m[1] * matrix.m[6] + m[2] * matrix.m[10] + m[3] * matrix.m[14];
-	mx.m[6] = m[4] * matrix.m[2] + m[5] * matrix.m[6] + m[6] * matrix.m[10] + m[7] * matrix.m[14];
-	mx.m[10] = m[8] * matrix.m[2] + m[9] * matrix.m[6] + m[10] * matrix.m[10] + m[11] * matrix.m[14];
-	mx.m[14] = m[12] * matrix.m[2] + m[13] * matrix.m[6] + m[14] * matrix.m[10] + m[15] * matrix.m[14];
-	// row 3
-	mx.m[3] = m[0] * matrix.m[3] + m[1] * matrix.m[7] + m[2] * matrix.m[11] + m[3] * matrix.m[15];
-	mx.m[7] = m[4] * matrix.m[3] + m[5] * matrix.m[7] + m[6] * matrix.m[11] + m[7] * matrix.m[15];
-	mx.m[11] = m[8] * matrix.m[3] + m[9] * matrix.m[7] + m[10] * matrix.m[11] + m[11] * matrix.m[15];
-	mx.m[15] = m[12] * matrix.m[3] + m[13] * matrix.m[7] + m[14] * matrix.m[11] + m[15] * matrix.m[15];
+	mx.m[0] = m[0] * matrix.m[0] + m[4] * matrix.m[1] + m[8] * matrix.m[2] + m[12] * matrix.m[3];
+	mx.m[1] = m[1] * matrix.m[0] + m[5] * matrix.m[1] + m[9] * matrix.m[2] + m[13] * matrix.m[3];
+	mx.m[2] = m[2] * matrix.m[0] + m[6] * matrix.m[1] + m[10] * matrix.m[2] + m[14] * matrix.m[3];
+	mx.m[3] = m[3] * matrix.m[0] + m[7] * matrix.m[1] + m[11] * matrix.m[2] + m[15] * matrix.m[3];
+	mx.m[4] = m[0] * matrix.m[4] + m[4] * matrix.m[5] + m[8] * matrix.m[6] + m[12] * matrix.m[7];
+	mx.m[5] = m[1] * matrix.m[4] + m[5] * matrix.m[5] + m[9] * matrix.m[6] + m[13] * matrix.m[7];
+	mx.m[6] = m[2] * matrix.m[4] + m[6] * matrix.m[5] + m[10] * matrix.m[6] + m[14] * matrix.m[7];
+	mx.m[7] = m[3] * matrix.m[4] + m[7] * matrix.m[5] + m[11] * matrix.m[6] + m[15] * matrix.m[7];
+	mx.m[8] = m[0] * matrix.m[8] + m[4] * matrix.m[9] + m[8] * matrix.m[10] + m[12] * matrix.m[11];
+	mx.m[9] = m[1] * matrix.m[8] + m[5] * matrix.m[9] + m[9] * matrix.m[10] + m[13] * matrix.m[11];
+	mx.m[10] = m[2] * matrix.m[8] + m[6] * matrix.m[9] + m[10] * matrix.m[10] + m[14] * matrix.m[11];
+	mx.m[11] = m[3] * matrix.m[8] + m[7] * matrix.m[9] + m[11] * matrix.m[10] + m[15] * matrix.m[11];
+	mx.m[12] = m[0] * matrix.m[12] + m[4] * matrix.m[13] + m[8] * matrix.m[14] + m[12] * matrix.m[15];
+	mx.m[13] = m[1] * matrix.m[12] + m[5] * matrix.m[13] + m[9] * matrix.m[14] + m[13] * matrix.m[15];
+	mx.m[14] = m[2] * matrix.m[12] + m[6] * matrix.m[13] + m[10] * matrix.m[14] + m[14] * matrix.m[15];
+	mx.m[15] = m[3] * matrix.m[12] + m[7] * matrix.m[13] + m[11] * matrix.m[14] + m[15] * matrix.m[15];
 	return mx;
 }
 
@@ -51,9 +47,9 @@ void Matrix::operator *= (const Matrix & matrix){
 
 Vector3D Matrix::operator * (const Vector3D &v) const{
 	return Vector3D(
-		m[0] * v.x + m[1] * v.y + m[2] * v.z,
-		m[4] * v.x + m[5] * v.y + m[6] * v.z,
-		m[8] * v.x + m[9] * v.y + m[10] * v.z
+		m[0] * v.x + m[4] * v.y + m[8] * v.z,
+		m[1] * v.x + m[5] * v.y + m[9] * v.z,
+		m[2] * v.x + m[6] * v.y + m[10] * v.z
 		);
 }
 
@@ -127,27 +123,27 @@ static float det3x3(float a1, float a2, float a3, float b1, float b2, float b3, 
 Matrix Matrix::getInverseMatrix() const{
 	Matrix b;
 
-	b.m[0] =  det3x3(m[5], m[9], m[13], m[6], m[10], m[14], m[7], m[11], m[15]);
-	b.m[4] = -det3x3(m[4], m[8], m[12], m[6], m[10], m[14], m[7], m[11], m[15]);
-	b.m[8] =  det3x3(m[4], m[8], m[12], m[5], m[9], m[13], m[7], m[11], m[15]);
-	b.m[12] = -det3x3(m[4], m[8], m[12], m[5], m[9], m[13], m[6], m[10], m[14]);
+	b.m[0] =  det3x3(m[5], m[6], m[7], m[9], m[10], m[11], m[13], m[14], m[15]);
+	b.m[1] = -det3x3(m[1], m[2], m[3], m[9], m[10], m[11], m[13], m[14], m[15]);
+	b.m[2] =  det3x3(m[1], m[2], m[3], m[5], m[6], m[7], m[13], m[14], m[15]);
+	b.m[3] = -det3x3(m[1], m[2], m[3], m[5], m[6], m[7], m[9], m[10], m[11]);
 
-	b.m[1] = -det3x3(m[1], m[9], m[13], m[2], m[10], m[14], m[3], m[11], m[15]);
-	b.m[5] =  det3x3(m[0], m[8], m[12], m[2], m[10], m[14], m[3], m[11], m[15]);
-	b.m[9] = -det3x3(m[0], m[8], m[12], m[1], m[9], m[13], m[3], m[11], m[15]);
-	b.m[13] =  det3x3(m[0], m[8], m[12], m[1], m[9], m[13], m[2], m[10], m[14]);
+	b.m[4] = -det3x3(m[4], m[6], m[7], m[8], m[10], m[11], m[12], m[14], m[15]);
+	b.m[5] =  det3x3(m[0], m[2], m[3], m[8], m[10], m[11], m[12], m[14], m[15]);
+	b.m[6] = -det3x3(m[0], m[2], m[3], m[4], m[6], m[7], m[12], m[14], m[15]);
+	b.m[7] =  det3x3(m[0], m[2], m[3], m[4], m[6], m[7], m[8], m[10], m[11]);
 
-	b.m[2] =  det3x3(m[1], m[5], m[13], m[2], m[6], m[14], m[3], m[7], m[15]);
-	b.m[6] = -det3x3(m[0], m[4], m[12], m[2], m[6], m[14], m[3], m[7], m[15]);
-	b.m[10] =  det3x3(m[0], m[4], m[12], m[1], m[5], m[13], m[3], m[7], m[15]);
-	b.m[14] = -det3x3(m[0], m[4], m[12], m[1], m[5], m[13], m[2], m[6], m[14]);
+	b.m[8] =  det3x3(m[4], m[5], m[7], m[8], m[9], m[11], m[12], m[13], m[15]);
+	b.m[9] = -det3x3(m[0], m[1], m[3], m[8], m[9], m[11], m[12], m[13], m[15]);
+	b.m[10] =  det3x3(m[0], m[1], m[3], m[4], m[5], m[7], m[12], m[13], m[15]);
+	b.m[11] = -det3x3(m[0], m[1], m[3], m[4], m[5], m[7], m[8], m[9], m[11]);
 
-	b.m[3] = -det3x3(m[1], m[5], m[9], m[2], m[6], m[10], m[3], m[7], m[11]);
-	b.m[7] =  det3x3(m[0], m[4], m[8], m[2], m[6], m[10], m[3], m[7], m[11]);
-	b.m[11] = -det3x3(m[0], m[4], m[8], m[1], m[5], m[9], m[3], m[7], m[11]);
-	b.m[15] =  det3x3(m[0], m[4], m[8], m[1], m[5], m[9], m[2], m[6], m[10]);
+	b.m[12] = -det3x3(m[4], m[5], m[6], m[8], m[9], m[10], m[12], m[13], m[14]);
+	b.m[13] =  det3x3(m[0], m[1], m[2], m[8], m[9], m[10], m[12], m[13], m[14]);
+	b.m[14] = -det3x3(m[0], m[1], m[2], m[4], m[5], m[6], m[12], m[13], m[14]);
+	b.m[15] =  det3x3(m[0], m[1], m[2], m[4], m[5], m[6], m[8], m[9], m[10]);
 
-	double det = (m[0] * b.m[0]) + (m[1] * b.m[4]) + (m[2] * b.m[8]) + (m[3] * b.m[12]);
+	double det = (m[0] * b.m[0]) + (m[4] * b.m[1]) + (m[8] * b.m[2]) + (m[12] * b.m[3]);
 
 	double epsilon = DBL_EPSILON;
 	if (det + epsilon >= 0.0f && det - epsilon <= 0.0f) {
@@ -157,23 +153,23 @@ Matrix Matrix::getInverseMatrix() const{
 	float oodet = (float) (1.0 / det);
 
 	b.m[0] *= oodet;
-	b.m[4] *= oodet;
-	b.m[8] *= oodet;
-	b.m[12] *= oodet;
-
 	b.m[1] *= oodet;
-	b.m[5] *= oodet;
-	b.m[9] *= oodet;
-	b.m[13] *= oodet;
-
 	b.m[2] *= oodet;
-	b.m[6] *= oodet;
-	b.m[10] *= oodet;
-	b.m[14] *= oodet;
-
 	b.m[3] *= oodet;
+
+	b.m[4] *= oodet;
+	b.m[5] *= oodet;
+	b.m[6] *= oodet;
 	b.m[7] *= oodet;
+
+	b.m[8] *= oodet;
+	b.m[9] *= oodet;
+	b.m[10] *= oodet;
 	b.m[11] *= oodet;
+
+	b.m[12] *= oodet;
+	b.m[13] *= oodet;
+	b.m[14] *= oodet;
 	b.m[15] *= oodet;
 
 	return b;
@@ -181,37 +177,59 @@ Matrix Matrix::getInverseMatrix() const{
 
 
 // transform the position by the matrix
-// (x,y,z,1) = (x,y,z,1)*[Matrix44]
+// (x,y,z) = (x,y,z)* rotation + translation
 // @param position float array for a vector3d
 void Matrix::transform(float *position) const{
-	float x,y,z;
-	x = m[0] * position[0] + m[1] * position[1] + m[2] * position[2];
-	y =	m[4] * position[0] + m[5] * position[1] + m[6] * position[2];
-	z = m[8] * position[0] + m[9] * position[1] + m[10] * position[2];
-	position[0] = x;
-	position[1] = y;
-	position[2] = z;
+	float x = position[0];
+	float y = position[1];
+	float z = position[2];
+	position[0] = m[0] * x + m[4] * y + m[8] * z + m[12];
+	position[1] = m[1] * x + m[5] * y + m[9] * z + m[13];
+	position[2] = m[2] * x + m[6] * y + m[10] * z + m[14];
+}
+
+void Matrix::transform(Vector3D & position) const{
+	float x = position.x;
+	float y = position.y;
+	float z = position.z;
+	position.x = m[0] * x + m[4] * y + m[8] * z + m[12];
+	position.y = m[1] * x + m[5] * y + m[9] * z + m[13];
+	position.z = m[2] * x + m[6] * y + m[10] * z + m[14];
 }
 
 // get vector transformed by the matrix
-//  (x,y,z,1)*[Matrix44]
+//  (x,y,z) = (x,y,z) * rotation
 // @param v a vector3d to be transformed
 // @return a vector transformed by the matrix
-Vector3D Matrix::TransformVector(const Vector3D& v) const
+void Matrix::transformVector(float * v) const
 {
-	return Vector3D(
-		m[0] * v.x + m[1] * v.y + m[2] * v.z,
-		m[4] * v.x + m[5] * v.y + m[6] * v.z,
-		m[8] * v.x + m[9] * v.y + m[10] * v.z
-		);
+	float x = v[0];
+	float y = v[1];
+	float z = v[2];	
+	v[0] = m[0] * x + m[4] * y + m[8] * z;
+	v[1] = m[1] * x + m[5] * y + m[9] * z;
+	v[2] = m[2] * x + m[6] * y + m[10] * z;
 }
 
+// get vector transformed by the matrix
+//  (x,y,z) = (x,y,z) * rotation
+// @param v a vector3d to be transformed
+// @return a vector transformed by the matrix
+void Matrix::transformVector(Vector3D & v) const
+{
+	float x = v.x;
+	float y = v.y;
+	float z = v.z;	
+	v.x = m[0] * x + m[4] * y + m[8] * z;
+	v.y = m[1] * x + m[5] * y + m[9] * z;
+	v.z = m[2] * x + m[6] * y + m[10] * z;
+}
+
+// the same as transform
 Vector3D operator * (const Vector3D& v, const Matrix& m){
-	return Vector3D(
-		m[0] * v.x + m[1] * v.y + m[2] * v.z,
-		m[4] * v.x + m[5] * v.y + m[6] * v.z,
-		m[8] * v.x + m[9] * v.y + m[10] * v.z
-		);
+	Vector3D nv;
+	m.transform(nv);
+	return nv;
 }
 
 // multiply a float by a matrix
@@ -229,7 +247,7 @@ void Matrix::Rotate(const Quaternion & q){
 	Vector3D axis;
 	float angle;
 	q.ToAngleAxis(axis,angle);
-	(*this)*=(axis.x,axis.y,axis.z,angle);
+	(*this)*=AxisRotationMatrix(axis.x,axis.y,axis.z,angle);
 }
 
 // rotate the matrix with by angle vector (x,y,z)
@@ -259,18 +277,19 @@ Matrix Matrix::AxisRotationMatrix(float x, float y, float z, float angle)
 	float sT = sinf(angle);
 
 	matrix[0] = xSq + (ySq + zSq) * cT;
-	matrix[4] = a.x * a.y * (1.0f - cT) + a.z * sT;
-	matrix[8] = a.x * a.z * (1.0f - cT) - a.y * sT;
-	matrix[12] = 0.0f;
-	matrix[1] = a.x * a.y * (1.0f - cT) - a.z * sT;
+	matrix[1] = a.x * a.y * (1.0f - cT) + a.z * sT;
+	matrix[2] = a.x * a.z * (1.0f - cT) - a.y * sT;
+	matrix[3] = 0;
+	matrix[4] = a.x * a.y * (1.0f - cT) - a.z * sT;
 	matrix[5] = ySq + (xSq + zSq) * cT;
-	matrix[9] = a.y * a.z * (1.0f - cT) + a.x * sT;
-	matrix[13] = 0.0f;
-	matrix[2] = a.x * a.z * (1.0f - cT) + a.y * sT;
-	matrix[6] = a.y * a.z * (1.0f - cT) - a.x * sT;
+	matrix[6] = a.y * a.z * (1.0f - cT) + a.x * sT;
+	matrix[7] = 0;
+	matrix[8] = a.x * a.z * (1.0f - cT) + a.y * sT;
+	matrix[9] = a.y * a.z * (1.0f - cT) - a.x * sT;
 	matrix[10] = zSq + (xSq + ySq) * cT;
-	matrix[14] = 0.0f;
-	matrix[11] = matrix[7] = matrix[3] = 0.0f;
-	matrix[15] = 1.0f;
+	matrix[11] = 0;
+	matrix[14] = matrix[13] = matrix[12] = 0;
+	matrix[15] = 1;
+
 	return matrix;
 }
